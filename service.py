24822156212dea0a9783c32c5280388e4ba93afe
Daniel Rocha <68558152+danroc@users.noreply.github.com>
2024-04-16 12:37:52 +0200

    chore: rename variables

--- service.py
+++ service.py
@@ -2,39 +2,39 @@ import os
 from geoblock import country_code, download_file, read_db
 
 
-IPV4_COUNTRY_DB_URL = (
+COUNTRY_IPV4_DB_URL = (
     "https://cdn.jsdelivr.net/npm/"
     + "@ip-location-db/"
     + "geolite2-country/"
     + "geolite2-country-ipv4.csv"
 )
 
-IPV6_COUNTRY_DB_URL = (
+COUNTRY_IPV6_DB_URL = (
     "https://cdn.jsdelivr.net/npm/"
     + "@ip-location-db/"
     + "geolite2-country/"
     + "geolite2-country-ipv6.csv"
 )
 
-IPV4_COUNTRY_DB_FILE = "geolite2-country-ipv4.csv"
+COUNTRY_IPV4_DB_FILE = "geolite2-country-ipv4.csv"
 
-IPV6_COUNTRY_DB_FILE = "geolite2-country-ipv6.csv"
+COUNTRY_IPV6_DB_FILE = "geolite2-country-ipv6.csv"
 
 
 class Service:
     def __init__(self):
-        if not os.path.exists(IPV4_COUNTRY_DB_FILE):
+        if not os.path.exists(COUNTRY_IPV4_DB_FILE):
             print("Downloading IPv4 database...")
-            download_file(IPV4_COUNTRY_DB_URL, IPV4_COUNTRY_DB_FILE, 10)
+            download_file(COUNTRY_IPV4_DB_URL, COUNTRY_IPV4_DB_FILE, 10)
 
-        if not os.path.exists(IPV6_COUNTRY_DB_FILE):
+        if not os.path.exists(COUNTRY_IPV6_DB_FILE):
             print("Downloading IPv6 database...")
-            download_file(IPV6_COUNTRY_DB_URL, IPV6_COUNTRY_DB_FILE, 10)
+            download_file(COUNTRY_IPV6_DB_URL, COUNTRY_IPV6_DB_FILE, 10)
 
         print("Reading databases...")
         self.databases = (
-            read_db(IPV4_COUNTRY_DB_FILE),
-            read_db(IPV6_COUNTRY_DB_FILE),
+            read_db(COUNTRY_IPV4_DB_FILE),
+            read_db(COUNTRY_IPV6_DB_FILE),
         )
 
         print("Ready!")
