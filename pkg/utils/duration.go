c61dbac68724b4cfb1f58264ce984c4dc4e0e150
Daniel Rocha <daniel@rocha.io>
2024-05-15 10:38:41 +0200

    feat: add duration parsing

--- pkg/utils/duration.go
+++ pkg/utils/duration.go
@@ -1,5 +1,84 @@
 package utils
 
-import "regexp"
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+)
 
-var reDuration = regexp.MustCompile(`(?P<duration>[1-9]\d*)(?P<unit>\w+)`)
+const (
+	TimeDay   time.Duration = 24 * time.Hour
+	TimeWeek                = 7 * TimeDay
+	TimeMonth               = 30 * TimeDay
+	TimeYear                = 365 * TimeDay
+)
+
+var (
+	reDurationSegment = regexp.MustCompile(`(\d+)([^\d\s]+)`)
+	reDurationString  = regexp.MustCompile(`^(\s*(\d+)\s*([^\d\s]+)\s*)+$`)
+)
+
+// ParseDuration parses a string representation of a duration and returns the
+// corresponding time.Duration value.
+//
+// The string should be in the format of a number followed by a unit, such as
+// "5s" for 5 seconds or "2h" for 2 hours. It can also contain multiple
+// durations that will be added together, such as "1h30m" for 1 hour and 30
+// minutes.
+//
+// Valid units are: "ns" (nanoseconds), "us" (microseconds), "ms"
+// (milliseconds), "s" (seconds), "m" (minutes), "h" (hours), "d" (days), "w"
+// (weeks), "M" (months), and "y" (years). If the string is not in a valid
+// format, an error is returned.
+func ParseDuration(s string) (time.Duration, error) {
+	// This check rejects negative values or invalid durations combined with
+	// valid ones. E.g. "-1s", "1m 30" or "1m s".
+	if !reDurationString.MatchString(s) {
+		return 0, fmt.Errorf("invalid duration: %s", s)
+	}
+
+	matches := reDurationSegment.FindAllStringSubmatch(
+		strings.ReplaceAll(s, " ", ""), -1,
+	)
+	if matches == nil {
+		return 0, fmt.Errorf("invalid duration: %s", s)
+	}
+
+	totalDuration := time.Duration(0)
+	for _, match := range matches {
+		duration, err := strconv.ParseInt(match[1], 10, 64)
+		if err != nil {
+			return 0, err
+		}
+
+		unit := match[2]
+		switch unit {
+		case "ns", "nanosecond", "nanoseconds":
+			totalDuration += time.Duration(duration) * time.Nanosecond
+		case "us", "microsecond", "microseconds":
+			totalDuration += time.Duration(duration) * time.Microsecond
+		case "ms", "millisecond", "milliseconds":
+			totalDuration += time.Duration(duration) * time.Millisecond
+		case "s", "second", "seconds":
+			totalDuration += time.Duration(duration) * time.Second
+		case "m", "minute", "minutes":
+			totalDuration += time.Duration(duration) * time.Minute
+		case "h", "hour", "hours":
+			totalDuration += time.Duration(duration) * time.Hour
+		case "d", "day", "days":
+			totalDuration += time.Duration(duration) * TimeDay
+		case "w", "week", "weeks":
+			totalDuration += time.Duration(duration) * TimeWeek
+		case "M", "month", "months":
+			totalDuration += time.Duration(duration) * TimeMonth
+		case "y", "year", "years":
+			totalDuration += time.Duration(duration) * TimeYear
+		default:
+			return 0, fmt.Errorf("invalid duration unit: %s", unit)
+		}
+	}
+
+	return totalDuration, nil
+}
---
+++ pkg/utils/duration_test.go
@@ -0,0 +1,61 @@
+package utils_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/danroc/geoblock/pkg/utils"
+)
+
+func TestParseDuration(t *testing.T) {
+	// Test valid durations
+	validDurations := map[string]time.Duration{
+		"0s":                  0,
+		"10s":                 10 * time.Second,
+		"1m30s":               90 * time.Second,
+		"2m":                  2 * time.Minute,
+		"03h":                 3 * time.Hour,
+		"3h30m":               3*time.Hour + 30*time.Minute,
+		"0h30m":               30 * time.Minute,
+		" 1h 30m ":            90 * time.Minute,
+		"4d":                  4 * utils.TimeDay,
+		"1w":                  1 * utils.TimeWeek,
+		"1M":                  1 * utils.TimeMonth,
+		"1y":                  1 * utils.TimeYear,
+		"1 minute":            1 * time.Minute,
+		"2 hours":             2 * time.Hour,
+		"1 day":               1 * utils.TimeDay,
+		"3 days":              3 * utils.TimeDay,
+		"1 minute 30 seconds": 90 * time.Second,
+	}
+
+	for input, expected := range validDurations {
+		duration, err := utils.ParseDuration(input)
+		if err != nil {
+			t.Errorf("Unexpected error for input '%s': %v", input, err)
+		}
+		if duration != expected {
+			t.Errorf("Incorrect duration for input '%s'. Expected: %v, Got: %v", input, expected, duration)
+		}
+	}
+
+	// Test invalid durations
+	invalidDurations := []string{
+		"-1s",
+		"1x",
+		"1.5h",
+		"1.5",
+		"1",
+		"x",
+		"1m 30",
+		"1m x",
+		"1m s",
+	}
+
+	for _, input := range invalidDurations {
+		_, err := utils.ParseDuration(input)
+		if err == nil {
+			t.Errorf("Expected error for invalid input '%s', but got no error", input)
+		}
+	}
+}
