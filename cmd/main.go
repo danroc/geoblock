e0822116ad706de26d30f4b5c77cef9845ea57e5
Daniel Rocha <daniel@rocha.io>
2024-10-25 13:31:49 +0200

    test: add engine unit tests

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"net/http"
 
+	"github.com/danroc/geoblock/pkg/database"
 	"github.com/danroc/geoblock/pkg/rules"
 	"github.com/danroc/geoblock/pkg/schema"
 )
@@ -32,78 +34,62 @@ type Service struct {
 func getAuthorize(
 	w http.ResponseWriter,
 	r *http.Request,
+	resolver *database.Resolver,
+	engine *rules.Engine,
 ) {
-	origins := r.Header[HeaderXForwardedFor]
+	origin := r.Header.Get(HeaderXForwardedFor)
+	domain := r.Header.Get(HeaderXForwardedHost)
 
-	// Block request: missing header
-	if origins == nil {
+	// Block the request if one or more of the required headers are missing. It
+	// probably means that the request didn't come from a reverse proxy.
+	if origin == "" || domain == "" {
 		w.WriteHeader(http.StatusForbidden)
 		return
 	}
 
-	// Find the country code for the client IP (first IP in the list)
+	sourceIP := net.ParseIP(origin)
+	resolution := resolver.Resolve(sourceIP)
 
-	// Block request: IP not found in the database
+	query := rules.Query{
+		RequestedDomain: domain,
+		SourceIP:        sourceIP,
+		SourceCountry:   resolution.CountryCode,
+		SourceASN:       resolution.ASN,
+	}
 
-	// Allow request: country code is in the allowed set
-	// w.WriteHeader(http.StatusOK)
-	// return
+	fmt.Printf("Query: %+v\n", query)
 
-	// Block request: default case
-	w.WriteHeader(http.StatusForbidden)
+	if engine.Authorize(query) {
+		w.WriteHeader(http.StatusOK)
+	} else {
+		w.WriteHeader(http.StatusForbidden)
+	}
 }
 
 func main() {
-	cfg, err := schema.ReadFile("examples/configuration.yaml")
+	config, err := schema.ReadFile("examples/configuration.yaml")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	engine := rules.NewEngine(config.AccessControl)
+
+	resolver, err := database.NewResolver()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	fmt.Printf("%+v\n", cfg)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/forward-auth", func(writer http.ResponseWriter, request *http.Request) {
+		getAuthorize(writer, request, resolver, engine)
+	})
 
-	engine := rules.NewEngine(cfg.AccessControl)
-	query := rules.Query{
-		RequestedDomain: "bc.gas.ovh",
-		SourceIP:        net.ParseIP("62.35.255.120"),
-		SourceCountry:   "US",
-		SourceASN:       1235,
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
 	}
 
-	if engine.Authorize(query) {
-		fmt.Println("Request authorized")
-	} else {
-		fmt.Println("Request denied")
-	}
-
-	// db, err := database.NewDatabase(countryIPv4URL)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
-
-	// resolver, err := database.NewResolver()
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
-
-	// match := resolver.Resolve(net.ParseIP("62.35.255.250"))
-	// fmt.Println(match)
-
-	// allowedCountryCodes := set.NewSet[string]()
-	// allowedCountryCodes.Add("FR")
-
-	// mux := http.NewServeMux()
-	// mux.HandleFunc("/v1/authorize", func(w http.ResponseWriter, r *http.Request) {
-	// 	getAuthorize(entries, allowedCountryCodes, w, r)
-	// })
-
-	// server := http.Server{
-	// 	Addr:    ":8080",
-	// 	Handler: mux,
-	// }
-
-	// log.Printf("Starting server at %s", server.Addr)
-	// log.Fatal(server.ListenAndServe())
+	log.Printf("Starting server at %s", server.Addr)
+	log.Fatal(server.ListenAndServe())
 }
---
+++ pkg/rules/engine_test.go
@@ -0,0 +1,201 @@
+package rules
+
+import (
+	"net"
+	"testing"
+
+	"github.com/danroc/geoblock/pkg/schema"
+)
+
+func TestEngine_Authorize(t *testing.T) {
+	tests := []struct {
+		name   string
+		config schema.AccessControl
+		query  Query
+		want   bool
+	}{
+		{
+			name: "allow by default policy",
+			config: schema.AccessControl{
+				Rules:         []schema.AccessControlRule{},
+				DefaultPolicy: schema.PolicyAllow,
+			},
+			query: Query{
+				RequestedDomain: "example.com",
+			},
+			want: true,
+		},
+		{
+			name: "deny by default policy",
+			config: schema.AccessControl{
+				Rules:         []schema.AccessControlRule{},
+				DefaultPolicy: schema.PolicyDeny,
+			},
+			query: Query{
+				RequestedDomain: "example.com",
+			},
+			want: false,
+		},
+		{
+			name: "allow by domain",
+			config: schema.AccessControl{
+				Rules: []schema.AccessControlRule{
+					{
+						Domains: []string{
+							"example.org",
+							"example.com",
+						},
+						Policy: schema.PolicyAllow,
+					},
+				},
+				DefaultPolicy: schema.PolicyDeny,
+			},
+			query: Query{
+				RequestedDomain: "example.com",
+			},
+			want: true,
+		},
+		{
+			name: "deny by domain",
+			config: schema.AccessControl{
+				Rules: []schema.AccessControlRule{
+					{
+						Domains: []string{
+							"example.org",
+							"example.com",
+						},
+						Policy: schema.PolicyDeny,
+					},
+				},
+				DefaultPolicy: schema.PolicyAllow,
+			},
+			query: Query{
+				RequestedDomain: "example.com",
+			},
+			want: false,
+		},
+		{
+			name: "allow by network",
+			config: schema.AccessControl{
+				Rules: []schema.AccessControlRule{
+					{
+						Networks: []schema.CIDR{
+							{IPNet: &net.IPNet{
+								IP:   net.IPv4(10, 0, 0, 0),
+								Mask: net.CIDRMask(8, 32),
+							}},
+							{IPNet: &net.IPNet{
+								IP:   net.IPv4(192, 168, 1, 0),
+								Mask: net.CIDRMask(24, 32),
+							}},
+						},
+						Policy: schema.PolicyAllow,
+					},
+				},
+				DefaultPolicy: schema.PolicyDeny,
+			},
+			query: Query{
+				SourceIP: net.IPv4(192, 168, 1, 1),
+			},
+			want: true,
+		},
+		{
+			name: "deny by network",
+			config: schema.AccessControl{
+				Rules: []schema.AccessControlRule{
+					{
+						Networks: []schema.CIDR{
+							{IPNet: &net.IPNet{
+								IP:   net.IPv4(10, 0, 0, 0),
+								Mask: net.CIDRMask(8, 32),
+							}},
+							{IPNet: &net.IPNet{
+								IP:   net.IPv4(192, 168, 1, 0),
+								Mask: net.CIDRMask(24, 32),
+							}},
+						},
+						Policy: schema.PolicyDeny,
+					},
+				},
+				DefaultPolicy: schema.PolicyAllow,
+			},
+			query: Query{
+				SourceIP: net.IPv4(192, 168, 1, 1),
+			},
+			want: false,
+		},
+		{
+			name: "allow by country",
+			config: schema.AccessControl{
+				Rules: []schema.AccessControlRule{
+					{
+						Countries: []string{"FR", "US"},
+						Policy:    schema.PolicyAllow,
+					},
+				},
+				DefaultPolicy: schema.PolicyDeny,
+			},
+			query: Query{
+				SourceCountry: "US",
+			},
+			want: true,
+		},
+		{
+			name: "deny by country",
+			config: schema.AccessControl{
+				Rules: []schema.AccessControlRule{
+					{
+						Countries: []string{"FR", "US"},
+						Policy:    schema.PolicyDeny,
+					},
+				},
+				DefaultPolicy: schema.PolicyAllow,
+			},
+			query: Query{
+				SourceCountry: "US",
+			},
+			want: false,
+		},
+		{
+			name: "allow by ASN",
+			config: schema.AccessControl{
+				Rules: []schema.AccessControlRule{
+					{
+						AutonomousSystems: []uint32{1111, 2222},
+						Policy:            schema.PolicyAllow,
+					},
+				},
+				DefaultPolicy: schema.PolicyDeny,
+			},
+			query: Query{
+				SourceASN: 2222,
+			},
+			want: true,
+		},
+		{
+			name: "deny by ASN",
+			config: schema.AccessControl{
+				Rules: []schema.AccessControlRule{
+					{
+						AutonomousSystems: []uint32{1111, 2222},
+						Policy:            schema.PolicyDeny,
+					},
+				},
+				DefaultPolicy: schema.PolicyAllow,
+			},
+			query: Query{
+				SourceASN: 2222,
+			},
+			want: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			e := NewEngine(tt.config)
+			if got := e.Authorize(tt.query); got != tt.want {
+				t.Errorf("Engine.Authorize() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
