6a7b4695556ef88dddc648cefab03ad82a3befa4
Daniel Rocha <68558152+danroc@users.noreply.github.com>
2024-10-25 19:01:06 +0200

    refactor: move server out of `main.go`

--- cmd/geoblock/main.go
+++ cmd/geoblock/main.go
@@ -1,84 +1,15 @@
 package main
 
 import (
-	"flag"
-	"net"
-	"net/http"
 	"os"
-	"time"
 
 	log "github.com/sirupsen/logrus"
 
 	"github.com/danroc/geoblock/pkg/database"
 	"github.com/danroc/geoblock/pkg/rules"
 	"github.com/danroc/geoblock/pkg/schema"
+	"github.com/danroc/geoblock/pkg/server"
 )
-
-const (
-	HeaderXForwardedMethod = "X-Forwarded-Method"
-	HeaderXForwardedProto  = "X-Forwarded-Proto"
-	HeaderXForwardedHost   = "X-Forwarded-Host"
-	HeaderXForwardedURI    = "X-Forwarded-Uri"
-	HeaderXForwardedFor    = "X-Forwarded-For"
-)
-
-type Rule struct {
-	Policy    string
-	Networks  []net.IPNet
-	Domains   []string
-	Countries []string
-}
-
-type Service struct {
-	DefaultPolicy string
-	Rules         []Rule
-}
-
-// getForwardAuth checks if the request is authorized to access the requested
-// resource. It uses the reverse proxy headers to determine the source IP and
-// requested domain.
-func getForwardAuth(
-	writer http.ResponseWriter,
-	request *http.Request,
-	resolver *database.Resolver,
-	engine *rules.Engine,
-) {
-	origin := request.Header.Get(HeaderXForwardedFor)
-	domain := request.Header.Get(HeaderXForwardedHost)
-
-	// Block the request if one or more of the required headers are missing. It
-	// probably means that the request didn't come from the reverse proxy.
-	if origin == "" || domain == "" {
-		writer.WriteHeader(http.StatusForbidden)
-		return
-	}
-
-	sourceIP := net.ParseIP(origin)
-	resolution := resolver.Resolve(sourceIP)
-
-	query := rules.Query{
-		RequestedDomain: domain,
-		SourceIP:        sourceIP,
-		SourceCountry:   resolution.CountryCode,
-		SourceASN:       resolution.ASN,
-	}
-
-	logFields := log.Fields{
-		"requested_domain": domain,
-		"source_ip":        sourceIP,
-		"source_country":   resolution.CountryCode,
-		"source_asn":       resolution.ASN,
-		"source_org":       resolution.Organization,
-	}
-
-	if engine.Authorize(&query) {
-		log.WithFields(logFields).Info("Request authorized")
-		writer.WriteHeader(http.StatusNoContent)
-	} else {
-		log.WithFields(logFields).Warn("Request denied")
-		writer.WriteHeader(http.StatusForbidden)
-	}
-}
 
 func getEnv(key, fallback string) string {
 	if value := os.Getenv(key); value != "" {
@@ -87,37 +18,16 @@ func getEnv(key, fallback string) string {
 	return fallback
 }
 
-type commandFlags struct {
+type appOptions struct {
 	configPath string
 	serverPort string
 }
 
-const (
-	DefaultConfigPath = "config/geoblock.yaml"
-	DefaultServerPort = "8080"
-)
-
-const usage = `Usage: geoblock [options]
-
-Options:
-  -c, --config CONFIG  Path to the configuration file (default "config/geoblock.yaml")
-  -p, --port   PORT    Port to run the server on (default "8080")`
-
-func parseFlags() *commandFlags {
-	flags := &commandFlags{}
-
-	flag.StringVar(&flags.configPath, "c", DefaultConfigPath, "")
-	flag.StringVar(&flags.configPath, "config", DefaultConfigPath, "")
-
-	flag.StringVar(&flags.serverPort, "p", DefaultServerPort, "")
-	flag.StringVar(&flags.serverPort, "port", DefaultServerPort, "")
-
-	flag.Usage = func() {
-		println(usage)
+func getOptions() *appOptions {
+	return &appOptions{
+		configPath: getEnv("GEOBLOCK_CONFIG", "config/geoblock.yaml"),
+		serverPort: getEnv("GEOBLOCK_PORT", "8080"),
 	}
-
-	flag.Parse()
-	return flags
 }
 
 func main() {
@@ -125,15 +35,10 @@ func main() {
 		FullTimestamp: true,
 	})
 
-	flags := parseFlags()
-
-	// var (
-	// 	configPath = getEnv("GEOBLOCK_CONFIG", "config/geoblock.yaml")
-	// 	serverPort = getEnv("GEOBLOCK_PORT", "8080")
-	// )
+	options := getOptions()
 
 	log.Info("Loading configuration file")
-	config, err := schema.ReadFile(flags.configPath)
+	config, err := schema.ReadFile(options.configPath)
 	if err != nil {
 		log.Fatalf("Failed to read configuration file: %v", err)
 	}
@@ -144,22 +49,11 @@ func main() {
 		log.Fatalf("Cannot initialize database resolver: %v", err)
 	}
 
-	engine := rules.NewEngine(&config.AccessControl)
-	mux := http.NewServeMux()
-	mux.HandleFunc(
-		"/v1/forward-auth",
-		func(writer http.ResponseWriter, request *http.Request) {
-			getForwardAuth(writer, request, resolver, engine)
-		},
+	var (
+		address = ":" + options.serverPort
+		engine  = rules.NewEngine(&config.AccessControl)
+		server  = server.NewServer(address, engine, resolver)
 	)
-
-	server := http.Server{
-		Addr:         ":" + flags.serverPort,
-		Handler:      mux,
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 60 * time.Second,
-		IdleTimeout:  60 * time.Second,
-	}
 
 	log.Infof("Starting server at %s", server.Addr)
 	log.Fatal(server.ListenAndServe())
---
+++ pkg/server/server.go
@@ -0,0 +1,89 @@
+package server
+
+import (
+	"net"
+	"net/http"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/danroc/geoblock/pkg/database"
+	"github.com/danroc/geoblock/pkg/rules"
+)
+
+const (
+	HeaderXForwardedMethod = "X-Forwarded-Method"
+	HeaderXForwardedProto  = "X-Forwarded-Proto"
+	HeaderXForwardedHost   = "X-Forwarded-Host"
+	HeaderXForwardedURI    = "X-Forwarded-Uri"
+	HeaderXForwardedFor    = "X-Forwarded-For"
+)
+
+// getForwardAuth checks if the request is authorized to access the requested
+// resource. It uses the reverse proxy headers to determine the source IP and
+// requested domain.
+func getForwardAuth(
+	writer http.ResponseWriter,
+	request *http.Request,
+	resolver *database.Resolver,
+	engine *rules.Engine,
+) {
+	origin := request.Header.Get(HeaderXForwardedFor)
+	domain := request.Header.Get(HeaderXForwardedHost)
+
+	// Block the request if one or more of the required headers are missing. It
+	// probably means that the request didn't come from the reverse proxy.
+	if origin == "" || domain == "" {
+		writer.WriteHeader(http.StatusForbidden)
+		return
+	}
+
+	sourceIP := net.ParseIP(origin)
+	resolution := resolver.Resolve(sourceIP)
+
+	query := rules.Query{
+		RequestedDomain: domain,
+		SourceIP:        sourceIP,
+		SourceCountry:   resolution.CountryCode,
+		SourceASN:       resolution.ASN,
+	}
+
+	logFields := log.Fields{
+		"requested_domain": domain,
+		"source_ip":        sourceIP,
+		"source_country":   resolution.CountryCode,
+		"source_asn":       resolution.ASN,
+		"source_org":       resolution.Organization,
+	}
+
+	if engine.Authorize(&query) {
+		log.WithFields(logFields).Info("Request authorized")
+		writer.WriteHeader(http.StatusNoContent)
+	} else {
+		log.WithFields(logFields).Warn("Request denied")
+		writer.WriteHeader(http.StatusForbidden)
+	}
+}
+
+// NewServer creates a new HTTP server that listens on the given address.
+func NewServer(
+	address string,
+	engine *rules.Engine,
+	resolver *database.Resolver,
+) *http.Server {
+	mux := http.NewServeMux()
+	mux.HandleFunc(
+		"/v1/forward-auth",
+		func(writer http.ResponseWriter, request *http.Request) {
+			getForwardAuth(writer, request, resolver, engine)
+		},
+	)
+
+	return &http.Server{
+		Addr:         address,
+		Handler:      mux,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 60 * time.Second,
+		IdleTimeout:  60 * time.Second,
+	}
+}
