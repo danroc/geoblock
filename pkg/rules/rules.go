98b08a01a0a624855429b81d5c1e6445876850e5
Daniel Rocha <daniel@rocha.io>
2024-10-25 08:52:54 +0200

    wip: rules

--- pkg/rules/rules.go
+++ pkg/rules/rules.go
@@ -6,41 +6,81 @@ import (
 	"github.com/danroc/geoblock/pkg/configuration"
 )
 
-type rule struct {
-	policy    string
-	networks  []net.IPNet
-	domains   []string
-	countries []string
-	asn       []uint32
-}
-
 type Engine struct {
-	defaultPolicy string
-	rules         []rule
+	config configuration.AccessControl
 }
 
-func NewEngine(config configuration.Configuration) (*Engine, error) {
-	controller := &Engine{
-		defaultPolicy: config.AccessControl.DefaultPolicy,
-		rules:         make([]rule, len(config.AccessControl.Rules)),
+func NewEngine(config configuration.Configuration) *Engine {
+	return &Engine{
+		config: config.AccessControl,
 	}
+}
 
-	for i, r := range config.AccessControl.Rules {
-		controller.rules[i] = rule{
-			policy:    r.Policy,
-			networks:  make([]net.IPNet, len(r.Networks)),
-			domains:   r.Domains,
-			countries: r.Countries,
-			asn:       r.AutonomousSystems,
-		}
+type Query struct {
+	RequestedDomain string
+	SourceIP        net.IP
+	SourceCountry   string
+	SourceASN       uint32
+}
 
-		for j, network := range r.Networks {
-			_, ipNet, err := net.ParseCIDR(network)
-			if err != nil {
-				return nil, err
+// ruleApplies checks if the given query is allowed or denied by the given
+// rule. For a rule to be applicable, the query must match all of the rule's
+// conditions.
+func ruleApplies(query Query, rule configuration.AccessControlRule) bool {
+	// Check country rules
+	if len(rule.Countries) > 0 {
+		found := false
+		for _, country := range rule.Countries {
+			if country == query.SourceCountry {
+				found = true
+				break
 			}
-			controller.rules[i].networks[j] = *ipNet
+		}
+
+		if !found {
+			return false
 		}
 	}
-	return controller, nil
+
+	// Check domain rules
+	if len(rule.Domains) > 0 {
+		found := false
+		for _, domain := range rule.Domains {
+			if domain == query.RequestedDomain {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			return false
+		}
+	}
+
+	// Check ASN rules
+	if len(rule.AutonomousSystems) > 0 {
+		found := false
+		for _, asn := range rule.AutonomousSystems {
+			if asn == query.SourceASN {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (e *Engine) Authorize(q Query) bool {
+	for _, rule := range e.config.Rules {
+		if ruleApplies(q, rule) {
+			return rule.Policy == configuration.PolicyAllow
+		}
+	}
+
+	return e.config.DefaultPolicy == configuration.PolicyAllow
 }
