330932d91248faf4ead77276732ec840ff5c70c6
Daniel Rocha <68558152+danroc@users.noreply.github.com>
2024-10-31 20:03:59 +0100

    chore: update timeouts

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -94,8 +94,8 @@ func NewServer(
 	return &http.Server{
 		Addr:         address,
 		Handler:      mux,
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 60 * time.Second,
-		IdleTimeout:  60 * time.Second,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+		IdleTimeout:  30 * time.Second,
 	}
 }
