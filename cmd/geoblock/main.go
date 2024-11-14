56fee99cab3e31ca94e77515fc3df494f8200518
Daniel Rocha <68558152+danroc@users.noreply.github.com>
2024-11-14 23:44:03 +0100

    feat: auto-update databases and auto-reload configuration

--- cmd/geoblock/main.go
+++ cmd/geoblock/main.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"os"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 
@@ -10,6 +11,11 @@ import (
 	"github.com/danroc/geoblock/pkg/rules"
 	"github.com/danroc/geoblock/pkg/schema"
 	"github.com/danroc/geoblock/pkg/server"
+)
+
+const (
+	autoUpdateInterval = 24 * time.Hour
+	autoReloadInterval = 5 * time.Second
 )
 
 func getEnv(key, fallback string) string {
@@ -31,6 +37,55 @@ func getOptions() *appOptions {
 	}
 }
 
+// autoUpdate updates the databases at regular intervals.
+func autoUpdate(resolver *database.Resolver) {
+	for range time.Tick(autoUpdateInterval) {
+		if err := resolver.Update(); err != nil {
+			log.Errorf("Cannot update databases: %v", err)
+			continue
+		}
+		log.Info("Databases updated")
+	}
+}
+
+// hasChanged returns true if the two file infos are different. It only checks
+// the size and the modification time.
+func hasChanged(a, b os.FileInfo) bool {
+	return a.Size() != b.Size() || a.ModTime() != b.ModTime()
+}
+
+// autoReload watches the configuration file for changes and updates the engine
+// when it happens.
+func autoReload(engine *rules.Engine, config string) {
+	prevStat, err := os.Stat(config)
+	if err != nil {
+		log.Errorf("Cannot watch configuration file: %v", err)
+		return
+	}
+
+	for range time.Tick(autoReloadInterval) {
+		stat, err := os.Stat(config)
+		if err != nil {
+			log.Errorf("Cannot watch configuration file: %v", err)
+			continue
+		}
+
+		if !hasChanged(prevStat, stat) {
+			continue
+		}
+		prevStat = stat
+
+		config, err := schema.ReadFile(config)
+		if err != nil {
+			log.Errorf("Cannot read configuration file: %v", err)
+			continue
+		}
+
+		engine.UpdateConfig(&config.AccessControl)
+		log.Info("Configuration reloaded")
+	}
+}
+
 func main() {
 	log.SetFormatter(&log.TextFormatter{
 		FullTimestamp: true,
@@ -41,7 +96,7 @@ func main() {
 	log.Info("Loading configuration file")
 	config, err := schema.ReadFile(options.configPath)
 	if err != nil {
-		log.Fatalf("Failed to read configuration file: %v", err)
+		log.Fatalf("Cannot read configuration file: %v", err)
 	}
 
 	log.Info("Initializing database resolver")
@@ -55,6 +110,9 @@ func main() {
 		engine  = rules.NewEngine(&config.AccessControl)
 		server  = server.NewServer(address, engine, resolver)
 	)
+
+	go autoUpdate(resolver)
+	go autoReload(engine, options.configPath)
 
 	log.Infof("Starting server at %s", server.Addr)
 	log.Fatal(server.ListenAndServe())
