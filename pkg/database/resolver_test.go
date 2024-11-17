0c2366854747e8bd319fe7356dcc8473125d5899
Daniel Rocha <68558152+danroc@users.noreply.github.com>
2024-11-17 03:41:05 +0100

    test: add resolver unit tests

--- pkg/database/resolver_test.go
+++ pkg/database/resolver_test.go
@@ -1,6 +1,10 @@
 package database_test
 
 import (
+	"bytes"
+	"io"
+	"net"
+	"net/http"
 	"testing"
 
 	"github.com/danroc/geoblock/pkg/database"
@@ -51,4 +55,87 @@ func TestStrToASN(t *testing.T) {
 			}
 		})
 	}
+}
+
+type mockRT struct {
+	respond func(req *http.Request) (*http.Response, error)
+}
+
+func (m *mockRT) RoundTrip(req *http.Request) (*http.Response, error) {
+	return m.respond(req)
+}
+
+func newDummyRT() http.RoundTripper {
+	return &mockRT{
+		respond: func(req *http.Request) (*http.Response, error) {
+			body := map[string]string{
+				database.CountryIPv4URL: "1.0.0.0,1.0.2.2,US\n1.1.0.0,1.1.2.2,FR\n",
+				database.CountryIPv6URL: "1:0::,1:1::,US\n1:2::,1:3::,FR\n",
+				database.ASNIPv4URL:     "1.0.0.0,1.0.2.2,1,Test1\n1.1.0.0,1.1.2.2,2,Test2\n",
+				database.ASNIPv6URL:     "1:0::,1:1::,3,Test3\n1:2::,1:3::,4,Test4\n",
+			}[req.URL.String()]
+
+			return &http.Response{
+				StatusCode: http.StatusOK,
+				Body:       io.NopCloser(bytes.NewBufferString(body)),
+			}, nil
+		},
+	}
+}
+
+func newErrRT() http.RoundTripper {
+	return &mockRT{
+		respond: func(req *http.Request) (*http.Response, error) {
+			return nil, io.ErrUnexpectedEOF
+		},
+	}
+}
+
+func withRT(rt http.RoundTripper, f func()) {
+	original := http.DefaultTransport
+	http.DefaultTransport = rt
+	defer func() { http.DefaultTransport = original }()
+	f()
+}
+
+func TestNewResolverError(t *testing.T) {
+	withRT(newErrRT(), func() {
+		_, err := database.NewResolver()
+		if err == nil {
+			t.Fatal("expected an error, got nil")
+		}
+	})
+}
+
+func TestResolverResolve(t *testing.T) {
+	withRT(newDummyRT(), func() {
+		tests := []struct {
+			ip      string
+			country string
+			org     string
+			asn     uint32
+		}{
+			{"1.0.1.1", "US", "Test1", 1},
+			{"1.1.1.1", "FR", "Test2", 2},
+			{"1.2.1.1", "", "", database.ReservedAS0},
+			{"1:0::", "US", "Test3", 3},
+			{"1:2::", "FR", "Test4", 4},
+			{"1:4::", "", "", database.ReservedAS0},
+		}
+		r, _ := database.NewResolver()
+		for _, tt := range tests {
+			t.Run(tt.ip, func(t *testing.T) {
+				result := r.Resolve(net.ParseIP(tt.ip))
+				if result.CountryCode != tt.country {
+					t.Errorf("got %q, want %q", result.CountryCode, tt.country)
+				}
+				if result.ASN != tt.asn {
+					t.Errorf("got %q, want %q", result.ASN, tt.asn)
+				}
+				if result.Organization != tt.org {
+					t.Errorf("got %q, want %q", result.Organization, tt.org)
+				}
+			})
+		}
+	})
 }
