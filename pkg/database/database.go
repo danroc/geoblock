3bb808d7a64edf812f31ec619a7d232da962c46e
Daniel Rocha <68558152+danroc@users.noreply.github.com>
2024-11-17 03:57:36 +0100

    chore: rename `sanitizeData` to `sanitize` (#22)

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -20,8 +20,8 @@ type Entry struct {
 	Data    []string
 }
 
-// sanitizeData trims the leading and trailing spaces from the given strings.
-func sanitizeData(data []string) []string {
+// sanitize trims the leading and trailing spaces from the given strings.
+func sanitize(data []string) []string {
 	sanitized := make([]string, len(data))
 	for i, d := range data {
 		sanitized[i] = strings.TrimSpace(d)
@@ -36,7 +36,7 @@ func parseRecords(records [][]string) ([]Entry, error) {
 		var (
 			startIP = net.ParseIP(record[0])
 			endIP   = net.ParseIP(record[1])
-			data    = sanitizeData(record[2:])
+			data    = sanitize(record[2:])
 		)
 
 		if startIP == nil {
